a5053224604bc95411249c2d72f87ba0b40ef5b1
Mosunov Anton <antonmossunov@gmail.com>
2022-11-11 13:39:51 +0600

    Реализация вывода высказываний артистов на их детальной странице.
    Добавлен запрос на получение высказываний артистов

--- task_02/js/02_character_task.js
+++ task_02/js/02_character_task.js
@@ -5,11 +5,31 @@ $(document).ready(function() {
 
     $.get(`https://www.breakingbadapi.com/api/characters/${param}`, function(data) {
 
-        let cardProfile = $('<div class="card" style="width: 30rem; ' +
+        let actorName = `${data[0].name}`;
+        let splitActorName = actorName.split(" ")
+
+        $.get(`https://www.breakingbadapi.com/api/quote?author=${splitActorName[0]}+${splitActorName[1]}`, function(data) {
+            console.log(data)
+
+            let quotes = $('<div class="card mb-3" style="max-width: 40rem; margin: auto">\n' +
+                '  <div class="card-body">\n' +
+                '    <h5 class="card-title">Quotes</h5>\n' +
+                '    <p class="card-text" id="quotes"></p>\n' +
+                '  </div>\n' +
+                '</div>')
+
+            $('#container-card').append(quotes);
+            for(let i = 0; i < data.length; i++) {
+                $('#quotes').append(data[i].quote + '<br>')
+            }
+        })
+
+        let cardProfile = $('<div class="card" style="width: 40rem; ' +
             'margin: auto; margin-top: 50px; margin-bottom: 100px">\n' +
             '  <img src="..." id="img" class="card-img-top" alt="...">\n' +
             '  <div class="card-body">\n' +
-            '  <h2 class="card-text" id="keys" style="color: grey; font-size: 20px"></h2>\n' +
+             '    <h5 class="card-title">Actor detail</h5>\n' +
+            '  <h2 class="card-text" id="keys" style="color: grey; font-size: 15px"></h2>\n' +
             '  </div>\n' +
             '</div>');
 
